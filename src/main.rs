64017a6caba4ec4da50e0e5d779fa7ac4c4d4e5e
fomgleb <fomglib@gmail.com>
2024-04-21 08:20:43 +0300

    Add a yellow rect drawing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("SDL2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(sdl2::keyboard::Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        canvas.set_draw_color(Color::RGB(255, 255, 0));
+        canvas.fill_rect(Rect::new(50, 50, 200, 150)).unwrap();
+
+        canvas.present();
+        ::std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
