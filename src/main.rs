f155c69ed6982e4b49cbdbebd8b4936eeed91ed9
fomgleb <fomglib@gmail.com>
2024-04-23 19:56:04 +0300

    Move loop to run_main_loop function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use sdl2::event::Event;
 use sdl2::pixels::Color;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::{EventPump, VideoSubsystem};
 use std::error::Error;
 
-fn create_empty_window() -> Result<(), Box<dyn Error>> {
+struct SDLContext {
+    sdl_context: sdl2::Sdl,
+    video_subsystem: VideoSubsystem,
+    canvas: Canvas<Window>,
+    event_pump: EventPump,
+}
+
+fn create_empty_window() -> Result<SDLContext, Box<dyn Error>> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
@@ -12,12 +22,21 @@ fn create_empty_window() -> Result<(), Box<dyn Error>> {
         .build()?;
 
     let mut canvas = window.into_canvas().build()?;
-    let mut event_pump = sdl_context.event_pump()?;
+    let event_pump = sdl_context.event_pump()?;
 
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
     canvas.present();
 
+    Ok(SDLContext {
+        sdl_context,
+        video_subsystem,
+        canvas,
+        event_pump,
+    })
+}
+
+fn run_main_loop(event_pump: &mut EventPump) {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -31,12 +50,13 @@ fn create_empty_window() -> Result<(), Box<dyn Error>> {
         }
         std::thread::sleep(std::time::Duration::from_secs_f32(1.0 / 60.0));
     }
-
-    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    create_empty_window()?;
+    let mut sdl_context = create_empty_window()?;
+    run_main_loop(&mut sdl_context.event_pump);
+
+    // sdl_context is dropped here, which calls SDL_Quit()
 
     Ok(())
 }
