c71136b555b58b41b697fde35f7fac682bf5ae9e
fomgleb <fomglib@gmail.com>
2024-06-03 00:49:29 +0300

    Feature: Implement grid rendering and draggable field offset
    - Added constants for grid distances (GRID_DISTANCE_HORIZONTAL and GRID_DISTANCE_VERTICAL).
    - Introduced field `field_offset_is_changing` to manage dragging state.
    - Implemented `render_grid` method to draw grid lines.
    - Updated mouse event handling to support dragging and moving the field.
    - Optimized rendering loop to clear and redraw the grid when the field offset changes.

--- src/logic_circuit_window.rs
+++ src/logic_circuit_window.rs
@@ -6,6 +6,8 @@ use std::error::Error;
 
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
+const GRID_DISTANCE_HORIZONTAL: usize = 50;
+const GRID_DISTANCE_VERTICAL: usize = 50;
 
 pub struct LogicCircuitWindow {
     sdl_context: sdl2::Sdl,
@@ -15,6 +17,7 @@ pub struct LogicCircuitWindow {
 
     last_lmb_down_point: Point,
     field_offset: Point,
+    field_offset_is_changing: bool,
 }
 
 impl LogicCircuitWindow {
@@ -44,7 +47,44 @@ impl LogicCircuitWindow {
             event_pump,
             last_lmb_down_point,
             field_offset,
+            field_offset_is_changing: false,
         })
+    }
+
+    fn render_grid(&mut self) -> Result<(), Box<dyn Error>> {
+        self.canvas.set_draw_color(Color::BLACK);
+
+        let first_horizontal_line_y = (self.field_offset.y as f32
+            / GRID_DISTANCE_HORIZONTAL as f32)
+            .ceil() as i32
+            * GRID_DISTANCE_HORIZONTAL as i32
+            - self.field_offset.y;
+        let first_vertical_line_x = (self.field_offset.x as f32
+            / GRID_DISTANCE_VERTICAL as f32)
+            .ceil() as i32
+            * GRID_DISTANCE_VERTICAL as i32
+            - self.field_offset.x;
+
+        for y in (first_horizontal_line_y
+            ..(first_horizontal_line_y + WINDOW_HEIGHT as i32))
+            .step_by(GRID_DISTANCE_HORIZONTAL)
+        {
+            self.canvas.draw_line(
+                Point::new(0, y),
+                Point::new(WINDOW_WIDTH as i32, y),
+            )?;
+        }
+        for x in (first_vertical_line_x
+            ..(first_vertical_line_x + WINDOW_WIDTH as i32))
+            .step_by(GRID_DISTANCE_VERTICAL)
+        {
+            self.canvas.draw_line(
+                Point::new(x, 0),
+                Point::new(x, WINDOW_HEIGHT as i32),
+            )?;
+        }
+
+        Ok(())
     }
 
     pub fn run_main_loop(&mut self) {
@@ -61,19 +101,42 @@ impl LogicCircuitWindow {
                         x,
                         y,
                         ..
-                    } => self.last_lmb_down_point = Point::new(x, y),
+                    } => {
+                        self.last_lmb_down_point =
+                            Point::new(x, y) + self.field_offset;
+                        self.field_offset_is_changing = true;
+                    }
                     Event::MouseButtonUp {
                         mouse_btn: MouseButton::Left,
                         x,
                         y,
                         ..
                     } => {
-                        self.field_offset +=
-                            self.last_lmb_down_point.offset(-x, -y);
+                        self.field_offset_is_changing = false;
+                    }
+                    Event::MouseMotion { x, y, .. } => {
+                        if self.field_offset_is_changing {
+                            self.field_offset =
+                                self.last_lmb_down_point.offset(-x, -y);
+                        }
                     }
                     _ => {}
                 }
             }
+
+            if self.field_offset_is_changing {
+                self.canvas.set_draw_color(Color::RGB(255, 255, 255));
+                self.canvas.clear();
+                self.render_grid();
+                self.canvas.present();
+            }
+
+            println!(
+                "x: {}, y: {}",
+                self.field_offset.x(),
+                self.field_offset.y()
+            );
+
             std::thread::sleep(std::time::Duration::from_secs_f32(1.0 / 60.0));
         }
     }
