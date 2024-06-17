3a9da4586b6f387b1596f4163fec44242731e833
fomgleb <fomglib@gmail.com>
2024-06-17 21:50:56 +0300

    Add new fn for Renderer for rendering with resolution

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,4 @@
-use crate::renderable::Renderable;
+use crate::{renderable::Renderable, resolution::Resolution};
 use sdl2::{rect::Point, render::Canvas, video::Window};
 use std::{collections::LinkedList, error::Error};
 
@@ -20,6 +20,19 @@ impl Renderer {
     ) -> Result<(), Box<dyn Error>> {
         for r in self.renderables.iter() {
             r.render(canvas, field_offset)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn render_everything_with_resolution(
+        &self,
+        canvas: &mut Canvas<Window>,
+        field_offset: Point,
+        resolution: Resolution,
+    ) -> Result<(), Box<dyn Error>> {
+        for r in self.renderables.iter() {
+            r.render_with_resolution(canvas, field_offset, resolution)?;
         }
 
         Ok(())
